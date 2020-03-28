7da540341eb68a507bc619d1ff3425915dc019c7
Thijs Spijker <thijs@tsnetwork.nl>
2020-03-28 16:51:59 +0100

    add grid

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,5 +1,5 @@
 const cacheName = 'v1';
-const cacheAssets = ['/', '/offline', '/static/index-52cb9f654d.css', '/static/index-5f53e12011.js'];
+const cacheAssets = ['/', '/offline', '/static/index-345757e5e5.css', '/static/index-5f53e12011.js'];
 
 self.addEventListener('install', (event) => {
 	console.log('installing');
